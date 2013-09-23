20cfb32fab56a77c97cd96bf30530610e2bab0bb
Harry Schwartz <hschwartz@brightcove.com>
2013-09-23 16:11:06 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+### Pet Name Generator
+
+For when caring is just too hard.
---
+++ pet-names.rb
@@ -0,0 +1,41 @@
+#!/usr/bin/env ruby
+
+foods = %w{almond apple apricot artichoke asparagus aubergine avocado
+bacon bagel banana barley basil batter bean beef beer beets berry
+biscuit black coffee blackberry blueberry bologna bonbon brandy bread
+bread breadstick breast brioche broccoli brownie buckwheat butter
+buttermilk cabbage cake candy cappuccino caramels caraway cardamom
+carrot cashew cauliflower caviar celery cereal champagne cheddar
+cheese cheesecake cherry chestnut chicken chicken chili chocolate
+cider cinnamon clams cocktail cocoa coconut cod coffee cognac collards
+cookie cookie coriander corn corned-beef crab cracker cranberry cream
+cream-cheese cress croissant crouton cucumber cupcake currant cutlet
+dill dough doughnut dressing drumstick duck dumpling eel egg eggplant
+espresso fig fish flour frankfurter fritter fruitcake fudge garlic
+garlic gingerbread goose gooseberry gourd grape grapefruit gravy
+grease half-and-half halibut ham hamburger hard cheese hazelnut herb
+herring herring honey hot-dog ice-cream jam jelly-bean kefir ketchup
+kiwi lamb lard leek lemon lemonade lettuce lime liqueur lobster
+lollipop macadamia macaroni mackerel mango margarine marjoram
+marshmallow mayonnaise meat melon mild cheese milk milkshake mint
+mozzarella muesli muffin mushroom mustard mutton nectarine noodles nut
+nutmeg oat oil olive olive onion orange oregano oysters pancake papaya
+parmesan parsley pasta pastrami pastry pea peach peanut peanut-butter
+pear pecan pepper perch persimmon pickle pie pike pineapple pistachio
+pita pizza plum pomegranate popcorn pork potato poultry prawn pretzel
+prune pumpkin pumpkin radish raisin raspberry ravioli relish rice roll
+roquefort rosemary rye salami salmon salt sauerkraut sausage seafood
+sharp cheese shellfish shortcake shrimp sourdough soybean spaghetti
+spice spinach squash squid steak strawberry sturgeon sundae
+sweet-and-sour syrup taffy tangerine tarragon tart tea thyme toast
+toffee tomato torte tortilla trout tuna turkey turnip veal vermicelli
+vodka wafer waffle walnut watercress watermelon wheat whisky wiener
+wine yogurt zucchini}
+
+body_parts = %w{ankle arm arm back belly bottom breast bum butt calf
+calves cheek cheeks chin ear elbow eye eyebrow face feet finger
+fingers fist foot forehead hair hand head hip hips knee knees leg legs
+lip lips mouth neck nose nostril nostrils shoulder teeth thigh thumb
+tits toe toes tongue tooth waist wrist}
+
+puts "#{foods.sample}-#{body_parts.sample}"
